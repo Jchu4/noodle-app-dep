d34876f2375f07ed8017a0d22953a6008a64116a
Jchu4 <jchua@jeromes-mbp.lan>
2021-02-03 21:11:18 +0800

    Update index.js & include 404.html

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 import { createServer } from 'http';
 import { readFile } from 'fs';
+import { extname } from 'path';
 
 const PORT = process.argv[2];
 
 const whenIncomingRequest = (request, response) => {
   console.log('Request URL: ', request.url);
 
-  var filePath = '.' + request.url;
+  let filePath = '.' + request.url;
+  if (filePath === './') {
+    filePath = './home.html';
+  }
 
-  readFile(filePath, (error, content) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end(content, 'utf-8');
+  let extName = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+  let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        readFile("./404.html", (err, content) => {
+          response.writeHead(404, { 'Content-Type' : 'text/html' });
+          response.end(content, 'utf-8');
+        });
+      }
+      else {
+        response.writeHead(500);
+        response.end('Sorry, check site admin for error: ' + err.code + '..\n');
+      }
+    }
+    else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
+    }
+   
   });
 };
 
